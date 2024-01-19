96e56e3f1468ddb895c16e3961bba971d2d61b4b
EsmanurB <139315416+EsmanurB@users.noreply.github.com>
2024-01-19 11:36:19 +0300

    Add files via upload

---
+++ class_objects/class_objects/Program.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_objects
+{
+
+
+    /* class Dikdortgen
+       {
+           private int a, b;
+
+           public Dikdortgen(int a, int b)
+           {
+
+               this.a = a;
+               this.b = b;
+
+
+           }
+           public int AlanHesapla()
+           {
+
+
+               return a * b;
+
+           }
+           public int CevreHesapla()
+           {
+
+               return 2 * (a + b);
+           }
+
+       }
+    */
+
+    /* class Daire
+       {
+           private double r1;
+
+           public  Daire( double r1)   
+           {
+               this.r1 = r1;
+
+           }
+
+           public double DaireAlan()
+           {
+               return 3.14 * r1 * r1;
+
+           }
+
+           public double DaireCevresi()
+           {
+               return 2 * 3.14 * r1;
+
+           }
+
+       }
+       */
+
+    //  Ogrenci sinifini ve alanlarını oluşturunuz ..
+
+    /* public class OgrenciSinifi
+       {
+           public string ogrenci1;
+           public string ogrenci2;
+           public string ogrenci3;
+
+       }
+       */
+
+
+    internal class Program
+    {
+
+        /*   static void Main(string[] args)
+             {
+                 //// oluşturulan class tan nesne yapma: Örnek 1
+
+                 //Dikdortgen d = new Dikdortgen(9,8);
+
+                 //   Console.WriteLine("Dikdortgen alani : {0}", d.AlanHesapla());
+
+                 //   Console.WriteLine("Dikdortgen çevresi : {0}", d.CevreHesapla());
+
+
+
+              /* // Daire için nesne oluşturmaaaaa Örnek 2-
+
+                 Daire daire = new Daire(9);
+
+                 Console.WriteLine("Daire Alanı : ");
+
+                 Console.WriteLine(daire.DaireAlan());
+                 Console.WriteLine("=======================================");
+
+                 Console.WriteLine("Daire Çevresi :");
+                 Console.WriteLine(daire.DaireCevresi());
+
+
+
+                 Console.ReadLine(); 
+              */
+
+
+        /*    private static void Main(string[] args)
+               {
+                 // Ogrenci sinifini ve alanlarını oluşturunuz
+                    OgrenciSinifi ogrenciAdi = new OgrenciSinifi();
+
+                   ogrenciAdi.ogrenci1 = "Esmanur";
+                   ogrenciAdi.ogrenci2 = "Nazli";
+                   ogrenciAdi.ogrenci3 = "betul";
+
+                   Console.WriteLine("1.OGRENCİ ADI : {0}" , ogrenciAdi.ogrenci1);
+                   Console.WriteLine("2.OGRENCİ ADI : {0}", ogrenciAdi.ogrenci2);
+                   Console.WriteLine("3.OGRENCİ ADI : {0}", ogrenciAdi.ogrenci3 );
+                   Console.ReadLine(); 
+
+
+
+
+               } */
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
